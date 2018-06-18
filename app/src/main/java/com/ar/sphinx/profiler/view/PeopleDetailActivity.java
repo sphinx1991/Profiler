7293bd372d92d7f46e37e8506f9e626327698516
sPhinx <ankit.rajput@instavans.com>
2018-06-18 21:37:34 +0530

    feat: detail activity for profile done

---
+++ app/src/main/java/com/ar/sphinx/profiler/view/PeopleDetailActivity.java
@@ -0,0 +1,18 @@
+package com.ar.sphinx.profiler.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ar.sphinx.profiler.R;
+
+/**
+ * Created by sphinx on 18/06/18.
+ */
+public class PeopleDetailActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_detail);
+	}
+}
